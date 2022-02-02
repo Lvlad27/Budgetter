bb0b0bba10941155ddb928f124f4ea0cdcc733c0
Vlad Leontescu <Lvlad27@gmail.com>
2022-02-02 15:58:32 +0200

    feat: add smooth scroll using js

--- script.js
+++ script.js
@@ -30,3 +30,19 @@ document.addEventListener('keydown', function (e) {
 		closeModal();
 	}
 });
+
+//
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+	const s1coords = section1.getBoundingClientRect();
+
+	// Scrolling
+	// window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+	window.scrollTo({
+		left: s1coords.left + window.pageXOffset,
+		top: s1coords.top + window.pageYOffset,
+		behavior: 'smooth',
+	});
+});
