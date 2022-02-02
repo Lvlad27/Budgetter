fba7ce49dddeba1d2d6250bbdfd6697c92ced8ef
Vlad Leontescu <Lvlad27@gmail.com>
2022-02-02 23:11:13 +0200

    feat: add tabbed component

--- script.js
+++ script.js
@@ -31,18 +31,62 @@ document.addEventListener('keydown', function (e) {
 	}
 });
 
-//
+// Button scrolling
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function (e) {
 	const s1coords = section1.getBoundingClientRect();
 
-	// Scrolling
-	// window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+	// Smooth scroll for old browsers
+	window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
 	window.scrollTo({
 		left: s1coords.left + window.pageXOffset,
 		top: s1coords.top + window.pageYOffset,
 		behavior: 'smooth',
 	});
+
+	// Smooth scroll only in modern browsers
+	section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//////////////////
+// Page navigation
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+	console.log(e.target);
+	e.preventDefault();
+
+	// Matching strategy
+	if (e.target.classList.contains('nav__link')) {
+		const id = e.target.getAttribute('href');
+		console.log(id);
+		document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+	}
+});
+
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+	const clicked = e.target.closest('.operations__tab');
+
+	// Guard clause
+	if (!clicked) return;
+
+	// Remove active classes for both the tab and respectively content areas
+	tabs.forEach(t => t.classList.remove('operations__tab--active'));
+	tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+	// Activate tab
+	clicked.classList.add('operations__tab--active');
+
+	// Activate content area
+	console.log(clicked.dataset.tab);
+	document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
