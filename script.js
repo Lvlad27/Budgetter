ce91af980897647243eefaad838c33bc4dfaaecf
Vlad Leontescu <Lvlad27@gmail.com>
2022-02-03 13:26:53 +0200

    fix: add Intersection Observer API to sticky nav

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const nav = document.querySelector('.nav');
+const navHeight = nav.getBoundingClientRect().height;
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
@@ -108,12 +109,42 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 // Sticky navigation
+/*
 const initialCoords = section1.getBoundingClientRect();
-console.log(initialCoords);
 
 window.addEventListener('scroll', function () {
-	console.log(window.scrollY);
-
 	if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
 	else nav.classList.remove('sticky');
 });
+*/
+
+// Sticky navigation. Intersectio Observer API
+/*
+const obsCallback = function (entries, observer) {
+	entries.forEach(entry => {
+		console.log(entry);
+	});
+};
+
+const obsOptions = {
+	root: null,
+	threshold: [0, 0.2],
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(section1);
+*/
+
+const stickyNav = function (entries) {
+	const [entry] = entries;
+
+	if (!entry.isIntersecting) nav.classList.add('sticky');
+	else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+	root: null,
+	threshold: 0,
+	rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
